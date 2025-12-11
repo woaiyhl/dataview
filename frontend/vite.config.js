52476219e20e055bc8d2b904850ab3ce8e88e295
卢华伟 <hw.lu@cyber-insight.com>
2025-12-11 21:25:36 +0800

    集成代码定位插件

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { codeInspectorPlugin } from "code-inspector-plugin";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codeInspectorPlugin({
+      bundler: "vite",
+    }),
+  ],
   server: {
     proxy: {
       "/api": {
